12240c8565690991b09e0d05e9f315592f76420b
Anwar Knyane <arsenic69@outlook.com>
2018-09-17 00:07:27 +0300

    new

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,15 +15,16 @@ object Main extends App with LazyLogging {
     "\n1 - Match only when both stock price and volume match (Simple Exchange)." +
     "\n2 - Check price and volume balances for validity before processing orders (Advanced Exchange)." +
     "\n3 - Information about project." +
-    "\n0 - Exit")
+    "\n4 - Exit")
 
 
-  val settingChoice = new Scanner(System.in).nextInt()
+  val settingChoice: Int = new Scanner(System.in).nextInt()
+
   settingChoice match {
     case 1 => logger.info(s"Simple Exchange selected"); exchangeEngine = SimpleExchangeEngine(clients)
     case 2 => logger.info(s"Advanced Exchange selected"); exchangeEngine = ComplexExchangeEngine(clients)
     case 3 => logger.info(s"Information about project"); println("SimpleScalaProject v1 - 2018/09/16 - Anwar Knyane")
-    case 0 => logger.info(s"Exiting Program"); System.exit(0)
+    case 4 => logger.info(s"Exiting Program"); System.exit(0)
   }
   exchangeEngine.start()
   orders.foreach(o => exchangeEngine.handleOrder(o))
